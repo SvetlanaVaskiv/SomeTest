0d3db328c5d64b4aaf3cd5ac4e021863a1c953b6
Svetlana <svetanochka@gmail.com>
2021-12-23 19:33:50 +0200

    one more test

---
+++ src/ThirdTask/Quiz.js
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export const Quiz = () => {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <Link to="/first">Go to first task</Link>
+    </>
+  );
+};
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { render } from "react-dom";
+
+// Instruments
+
+// Pages
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+render(
+  <>
+    <App />
+  </>,
+  document.getElementById("root")
+);
